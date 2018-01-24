1a48d6214af5dd486e91ad1c1a1e011b3422e5ed
mbc <malcolmbalch@gmail.com>
2018-01-23 20:20:41 -0500

    websocket + gdax wrapper lib for connecting to public ws conn

--- ingester.go
+++ ingester.go
@@ -1,17 +1,56 @@
 package main
 
+import (
+	"fmt"
+	ws "github.com/gorilla/websocket"
+	gdax "github.com/preichenberger/go-gdax"
+)
+
 type Ingester struct {
 	PostgresClient *PostgresClient
 	Conf           *Configuration
+	Url            string
 }
 
 func (i *Ingester) Start() {
+	var wsDialer ws.Dialer
+	wsConn, _, err := wsDialer.Dial(i.Url, nil)
+	if err != nil {
+		println(err.Error())
+	}
 
+	subscribe := gdax.Message{
+		Type: "subscribe",
+		Channels: []gdax.MessageChannel{
+			gdax.MessageChannel{
+				Name: "ticker",
+				ProductIds: []string{
+					"ETH-USD",
+				},
+			},
+		},
+	}
+	if err := wsConn.WriteJSON(subscribe); err != nil {
+		println(err.Error())
+	}
+
+	message := gdax.Message{}
+	for true {
+		if err := wsConn.ReadJSON(&message); err != nil {
+			println(err.Error())
+			break
+		}
+		fmt.Println(message.Type)
+		if message.Type == "ticker" {
+			fmt.Println(message)
+		}
+	}
 }
 
 func NewIngester(conf *Configuration) *Ingester {
 	i := new(Ingester)
 	i.Conf = conf
+	i.Url = "wss://ws-feed.gdax.com"
 
 	// Postgres
 	/*
